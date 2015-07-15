b517257cf4c4d675b74d6fc32da338caba283c86
Matt Drollette <matt@drollette.com>
2015-07-15 11:45:38 -0500

    support OPTIONS requests

--- lib/request/sfRequest.class.php
+++ lib/request/sfRequest.class.php
@@ -22,12 +22,12 @@
  */
 abstract class sfRequest implements ArrayAccess
 {
-  const GET    = 'GET';
-  const POST   = 'POST';
-  const PUT    = 'PUT';
-  const PATCH  = 'PATCH';
-  const DELETE = 'DELETE';
-  const HEAD   = 'HEAD';
+  const GET     = 'GET';
+  const POST    = 'POST';
+  const PUT     = 'PUT';
+  const PATCH   = 'PATCH';
+  const DELETE  = 'DELETE';
+  const HEAD    = 'HEAD';
   const OPTIONS = 'OPTIONS';
 
   protected
